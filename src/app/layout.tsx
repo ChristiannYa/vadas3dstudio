87ba96e035789373a68702e5a0ff80690d73deaf
ChristiannYa <christian.ylopez02@gmail.com>
2025-05-03 23:42:09 -0500

    updated seo information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,18 +16,6 @@ export const metadata: Metadata = {
   title: "Vadas 3D Studio",
   description: "3D rendering studio based in Houston, TX",
   manifest: "/site.webmanifest",
-  icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
-      { url: "/icon.svg", type: "image/svg+xml" },
-      { url: "/icon.png", sizes: "96x96", type: "image/png" },
-    ],
-    apple: {
-      url: "/apple-icon.png",
-      sizes: "180x180",
-      type: "image/png",
-    },
-  },
   openGraph: {
     type: "website",
     url: "https://vadas3dstudio.com/",
